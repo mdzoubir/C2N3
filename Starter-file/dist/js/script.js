a0ec0018c1b47274d07002a5984265b77c29dd08
mohamed zoubir <simozoubir012@gmail.com>
2020-05-02 00:09:47 +0000

    change page test

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -3,14 +3,23 @@ var score = document.querySelector(".score");
 var question = document.querySelector(".question");
 var scoreCard = document.querySelector("#scoreCard");
 var button = document.getElementById("button");
-
-
+var ipt1 = document.querySelector(".inpp");
+// var ipt2 = document.getElementById("non");
+var prece = document.getElementById('prece');
+var scoreNem = document.getElementById('scoreNem');
+var scorePr = document.getElementById('scorepr');
+var para = document.getElementById('paragraph');
+var blo = document.getElementById('clicked');
+var dom = document.getElementsByClassName("preambule");
 
 
 
 var app = {
     questions: [{
             q: "Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+        },
+        {
+            q: "Quelle est votre température corporelle ?",
         },
         {
             q: "Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
@@ -32,6 +41,18 @@ var app = {
         },
         {
             q: "Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
+        },
+        {
+            q: "Actuellement, comment vous vous sentez ?",
+        },
+        {
+            q: "Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.",
+        },
+        {
+            q: "Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
+        },
+        {
+            q: "Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
         },
         {
             q: "Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
@@ -62,13 +83,45 @@ var app = {
         },
     ],
     index: 0,
-
     load: function() {
-        question.innerHTML = this.questions[this.index].q;
+        if (this.index < 21) {
+            question.innerHTML = this.questions[this.index].q;
+            button.disabled = true;
+            ipt1.innerHTML = "";
+            ipt1.innerHTML += `
+        <div>
+            <input type="radio" name="choice" id="oui" value="" class="lab">
+            <label for="oui">
+                <i class="fas fa-check"></i>
+                <span>Oui</span>
+             </label>
+        </div>
+        <div>
+        <input type="radio" name="choice" id="non" class="lab">
+        <label for="non">
+            <i class="fas fa-times""></i>
+            <span>Non</span>
+         </label>
+        </div>`;
+            if (this.index === 1) {
+                question.innerHTML = this.questions[this.index].q;
+                button.disabled = true;
+                ipt1.innerHTML = "";
+                ipt1.innerHTML = `
+                <input type ="number" min = "34" max = "42" placeholder = "34-42" id = "inputTom">
+                    <span id = "inputDeg" >
+                    degrés </span>`;
+            }
+        } else {
+
+        }
     },
+
     next: function() {
         this.index++;
         this.load();
+
+
     },
     previous: function() {
         this.index--;
@@ -76,35 +129,28 @@ var app = {
 
 
     },
-    score: 1,
-    scoreCard: function() {
-        // scoreCard.innerHTML = this.score + "/" + this.questions.length;
-
+    scoreB: function() {
+        scoreNem.style.width = (this.index + 1) * 100 / 22 + '%';
+        scorePr.innerHTML = this.index + 1 + "/" + this.questions.length
     },
-};
 
-window.onload = app.scoreCard();
+};
 window.onload = app.load();
+window.onload = app.scoreB();
+
 
 function next() {
     app.next();
-    document.getElementById('prece').style.visibility = "visible";
+    prece.style.visibility = 'visible';
+    app.scoreB()
 
 }
 
 function previous() {
     app.previous();
+    app.scoreB()
 }
 
-
-
-var para = document.getElementById('paragraph');
-
-var blo = document.getElementById('clicked');
-var dom = document.getElementsByClassName("preambule");
-
-
-blo.style.cursor = "pointer";
 
 function newFunction() {
     para.style.display = "none";
@@ -114,10 +160,18 @@ function newFunction() {
 
 
 
-button.disabled = true;
+
 
 function suiv() {
     button.disabled = false;
     button.style.opacity = 1;
-    button.style.cursor = "pointer";
+    button.style.cursor = "pointer"
 }
+ipt1.addEventListener('change', (event) => {
+    const input = event.target;
+    if (input.type === 'radio') {
+        button.disabled = false;
+    } else {
+        button.disabled = false;
+    }
+});
